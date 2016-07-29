0d67eabc2a1fa5988877409b7f9ae848e904bbd2
Alexey Buyanow <buyanov@stroytorgi.ru>
2016-07-29 12:58:05 +0300

    Make -e key optional

--- config/route.config.php
+++ config/route.config.php
@@ -7,7 +7,7 @@ use ZfPhinx\Controller\PhinxController;
 return [
     'test' => [
         'options' => [
-            'route'    => 'zfphinx test [-v|-vv|-vvv] [-q] [-n] -e ENVIRONMENT',
+            'route'    => 'zfphinx test [-v|-vv|-vvv] [-q] [-n] [-e]',
             'defaults' => [
                 'controller' => PhinxController::class,
                 'action'     => 'test',
@@ -25,7 +25,7 @@ return [
     ],
     'migrate' => [
         'options' => [
-            'route'    => 'zfphinx migrate [-v|-vv|-vvv] [-q] [-n] [-t] [-d] -e ENVIRONMENT',
+            'route'    => 'zfphinx migrate [-v|-vv|-vvv] [-q] [-n] [-t] [-d] [-e]',
             'defaults' => [
                 'controller' => PhinxController::class,
                 'action'     => 'migrate',
@@ -34,7 +34,7 @@ return [
     ],
     'rollback' => [
         'options' => [
-            'route'    => 'zfphinx rollback [-v|-vv|-vvv] [-q] [-n] [-t] [-d] -e ENVIRONMENT',
+            'route'    => 'zfphinx rollback [-v|-vv|-vvv] [-q] [-n] [-t] [-d] [-e]',
             'defaults' => [
                 'controller' => PhinxController::class,
                 'action'     => 'rollback',
@@ -43,7 +43,7 @@ return [
     ],
     'status' => [
         'options' => [
-            'route'    => 'zfphinx status [-v|-vv|-vvv] [-q] [-n] [-f] -e ENVIRONMENT',
+            'route'    => 'zfphinx status [-v|-vv|-vvv] [-q] [-n] [-f] [-e]',
             'defaults' => [
                 'controller' => PhinxController::class,
                 'action'     => 'status',
